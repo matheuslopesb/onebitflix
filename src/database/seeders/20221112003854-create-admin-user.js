6215a6bc25c06735f91a6a7a3b6c0f7610bc73e9
Matheus Lopes Brito <matfsdev@gmail.com>
2022-11-12 18:53:51 -0300

    Autenticação e tela de login

--- src/database/seeders/20221112003854-create-admin-user.js
+++ src/database/seeders/20221112003854-create-admin-user.js
@@ -3,25 +3,25 @@
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  async up (queryInterface) {
+  async up(queryInterface) {
     const hashedPassword = await bcrypt.hash("123456", 10);
 
     await queryInterface.bulkInsert('users', [
       {
-        first_name: 'Admin', 
-        last_name: 'User', 
-        phone: '555-5555', 
-        birth: '1990-01-01', 
-        email: 'admin@gmail.com', 
-        password: hashedPassword, 
-        role: 'admin', 
-        created_at: new Date(), 
+        first_name: 'Admin',
+        last_name: 'User',
+        phone: '555-5555',
+        birth: '1990-01-01',
+        email: 'admin@gmail.com',
+        password: hashedPassword,
+        role: 'admin',
+        created_at: new Date(),
         updated_at: new Date()
       }
     ])
   },
 
-  async down (queryInterface) {
+  async down(queryInterface) {
     await queryInterface.bulkDelete('People', null, { where: { email: "admin@gmail.com" } });
   }
 };
