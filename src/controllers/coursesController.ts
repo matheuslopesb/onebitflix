df345142a9773a32c8d25b041209632594e76fd3
Matheus Lopes Brito <teteu260505@gmail.com>
2022-11-30 23:59:39 -0300

    Obtendo 3 cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,18 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
+    // GET /courses/featured
+    async featured(req: Request, res: Response) {
+        try {
+            const featuredCourses = await courseService.getRandomFeaturedCourses();
+            return res.json(featuredCourses); 
+        } catch (error) {
+            if(error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    }, 
+
     // GET /courses/:id
     async show(req: Request, res: Response) {
         const { id } = req.params;
