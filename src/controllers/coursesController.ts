00d9a43f25786f2443112a2732f4929195735189
Matheus Lopes Brito <teteu260505@gmail.com>
2023-01-03 02:42:26 -0300

    Atualizando o endpoint de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,6 +1,9 @@
+import { AuthenticatedRequest } from './../middlewares/auth';
 import { Request, Response } from "express";
 import { getPaginationParams } from "../helpers/getPaginationParams";
 import { courseService } from "../services/courseService";
+import { likeService } from '../services/likeService';
+import { favoriteService } from '../services/favoriteService';
 
 export const coursesController = {
     // GET /courses/featured
@@ -43,12 +46,20 @@ export const coursesController = {
     }, 
 
     // GET /courses/:id
-    show: async (req: Request, res: Response) => {
-        const { id } = req.params;
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id;
+        const { id: courseId } = req.params;
 
         try {
-            const course = await courseService.findByIdWithEpisodes(id); 
-            res.json(course); 
+            const course = await courseService.findByIdWithEpisodes(courseId); 
+
+            if (!course) {
+                return res.status(404).json({ message: 'Curso não encontrado' }); 
+            }
+
+            const liked = await likeService.isLiked(userId, Number(courseId))
+            const favorited = await favoriteService.isFavorited(userId, Number(courseId))
+            return res.json({ ...course.get(), favorited, liked })
         } catch (error) {
             if(error instanceof Error) {
                 return res.status(400).json({ message: error.message })
