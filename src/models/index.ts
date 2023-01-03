94dbfe579a66085b81ef18edfb32a28098608f9d
Matheus Lopes Brito <teteu260505@gmail.com>
2023-01-03 02:15:12 -0300

    Criando o gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 import { User } from "./User";
 
 
@@ -9,6 +10,7 @@ Category.hasMany(Course, { as: "courses" })
 
 Course.belongsTo(Category)
 Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
 Course.hasMany(Episode, { as: "episodes" })
 Course.hasMany(Favorite, { as: "FavoritesUsers", foreignKey: 'course_id' })
 
@@ -18,6 +20,7 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
 User.hasMany(Favorite, { as: "FavoritesCourses", foreignKey: 'user_id' })
 
 export {
@@ -25,5 +28,6 @@ export {
     Course, 
     Episode, 
     Favorite, 
+    Like, 
     User
 }
